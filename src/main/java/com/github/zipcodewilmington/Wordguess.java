90a4b86113d4ed335984d58cfe799df473c6fd2f
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-03-03 08:22:06 -0500

    frame work has been created in Wordguess.java, methods have the skeleton now have to fill in the muscle

--- src/main/java/com/github/zipcodewilmington/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -14,7 +14,63 @@ public class Wordguess {
         game.runGame();
     }
 
+    // runs game
     public void runGame(){
+
+    }
+
+    // print array
+    public static void printArray(){
+
+    }
+
+    // prints welcome
+    public static void announceGame(){
+
+    }
+
+    // prints game over
+    public static void gameOver(){
+
+    }
+
+    // sets up char[] for secret word and guesses
+    public static void initializeGameState(){
+
+    }
+
+    // returns char from player input
+    public  static char getNextGuess(){
+
+    }
+
+    // returns boolean
+    public static boolean isWordGuessed(){
+
+    }
+
+    // returns boolean
+    public static boolean askToPlayAgain(){
+
+    }
+
+    // uses printArray to show player where they are at
+    public static void printCurrentState(){
+
+    }
+
+    // loops thru the word array, looking for the inputed guess, and replaces the "_" with the guesses char if found
+    public static void process(){
+
+    }
+
+    // prints happy message
+    public static void playerWon(){
+
+    }
+
+    // prints sad message
+    public static void playerLost(){
 
     }
 }
